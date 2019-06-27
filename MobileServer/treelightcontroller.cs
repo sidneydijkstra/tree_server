4c5a251396860940c5380a61fc58c2b232ecde71
sidneydijkstra <sidney0099@hotmail.com>
2019-06-28 00:53:18 +0200

    added null check for tree

--- MobileServer/treelightcontroller.cs
+++ MobileServer/treelightcontroller.cs
@@ -9,13 +9,19 @@ using System.Threading.Tasks;
 
 public static class TreeLightController{
     public static void blink(Setting_RGB _rgb, int _amount){
+        if (NetworkTree.treeConnection == null)
+            return;
         NetworkTree.treeConnection.send("BLK;" + _rgb.formatToString());
     }
     public static void set(Setting_RGB _rgb, bool _fade) {
+        if (NetworkTree.treeConnection == null)
+            return;
         NetworkTree.treeConnection.send("SET;" + _rgb.formatToString());
     }
 
     public static void party() {
+        if (NetworkTree.treeConnection == null)
+            return;
         NetworkTree.treeConnection.send("PAR");
     }
 }
